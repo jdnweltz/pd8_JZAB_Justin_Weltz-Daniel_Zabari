f69c72168a8d124288b785f68ffd22816ecffbae
Zabari <zabari.daniel@Gmail.com>
2014-06-03 14:47:33 -0400

    minor

--- Minion Algorithm/Minion.java
+++ Minion Algorithm/Minion.java
@@ -2,10 +2,16 @@ public class Minion{
     private int r;
     private int c;
     private Maze m;
-    public Minion(int row, int col, Maze maze){
+    private int rD;
+    private int cD;
+    private float p;
+    public Minion(int row, int col, Maze maze, int rDirection,int cDirection, float percentage){
 	r=row;
 	c=col;
 	m=maze;
+	rD=rDirection;
+	cD=cDirection;
+	p=percentage;
     }
     private char val(){return m[r][c];}
     private void act{
@@ -16,3 +22,4 @@ public class Minion{
     private void forward(){
 	int d=(int)(Math.random()*4)+1;
 	
+    }
